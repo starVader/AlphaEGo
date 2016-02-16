eb280ba077ce1975426ae9c583debe9220eb072b
Rakesh Raushan <starvader473@gmail.com>
2016-02-16 15:43:35 +0530

    Update with code coverage

--- newtest_test.go
+++ newtest_test.go
@@ -4,9 +4,11 @@ package newtest
 import "testing"
 
 
-
 const checkMark =  "\u2713"
 const ballotX = "\u2717"
+var tr Tweet = Tweet{User:"rakesh",Post_date:"2011-998-990",Message:"hello world"}
+var data chan Tweet = make(chan Tweet)  
+
 
 func TestFunction(t *testing.T) {
 	t.Log("Given the need to return a csv writer")
@@ -17,7 +19,7 @@ func TestFunction(t *testing.T) {
 			if err != nil  {
 				t.Fatal("\t Should have been a *csv.Writer",ballotX)
 			}
-			t.Log("Got the Csv Writer", checkMark)
+			t.Log("\tGot the Csv Writer", checkMark)
 		}
 	}
 	t.Log("Given the need for elastic client")
@@ -26,15 +28,43 @@ func TestFunction(t *testing.T) {
 		{
 			_,err := getClient()
 			if err != nil {
-				t.Fatal("Should have been a elastic search client",ballotX)
+				t.Fatal("\tShould have been a elastic search client",ballotX)
 			}
-			t.Log("Got the elastic search client",checkMark)
+			t.Log("\tGot the elastic search client",checkMark)
+		}
+	}
+	t.Log("function csvWriter should  to file") 
+	{
+		t.Logf("checking function for return") 
+		{
+			k,err := getwriter()
+			if err != nil {
+				t.Fatal("\t should have been a csvwriter", ballotX)
+			}
+			go tr.csvWriter(k,data)
+			data <- tr
+			t.Log("\t data successfully written to csv", checkMark)
+
+		}
+	}
+	t.Log("getReport function should get the report from elasticsearch")
+	{
+		t.Logf("cheking getreport")
+		{
+			q = Query{search_string: "arpit", search_field: "user"}
+			client, err := getClient()
+				if err != nil {
+					t.Logf("\tError getting client", ballotX)
+				}
+				getReport(client)
+				t.Log("\tGot report successfully", checkMark)
 		}
 	}
 }
 
+
 //Benchmark getclient  function
-func BenchmarkGetc(b *testing.B) {
+func BenchmarkGetclient(b *testing.B) {
      b.ResetTimer()
      for i := 0; i < b.N; i++ {
          getClient()
@@ -42,10 +72,11 @@ func BenchmarkGetc(b *testing.B) {
 }
 
 //Benchmark getwriter function
-func BenchmarkCsv(b *testing.B) {
+func BenchmarkGetcsv(b *testing.B) {
      b.ResetTimer()
      for i := 0; i < b.N; i++ {
          getwriter()
      }
 }
+
 
