4ba0aa48ca6fbfe2da455d42f99e9a943e1b584f
Rakesh Raushan <starvader473@gmail.com>
2016-03-09 13:30:54 +0530

    formatting

--- newtest.go
+++ newtest.go
@@ -6,9 +6,9 @@ import (
 	"fmt"
 	"gopkg.in/olivere/elastic.v3"
 	"os"
-	"sync"
 	"reflect"
 	"strings"
+	"sync"
 )
 
 //Sample output struct
@@ -24,7 +24,7 @@ type OUTPUT interface {
 }
 
 //Database Interface gets client according to database
-type GETCLIENT interface{
+type GETCLIENT interface {
 	GetClient()
 }
 
@@ -34,7 +34,7 @@ type Filter struct {
 }
 
 type Fpair struct {
-	Qkey  string
+	Qkey   string
 	Qvalue string
 }
 
@@ -43,30 +43,29 @@ type DatabaseType int
 
 //Constants of type DatabaseType
 const (
-	Elasticsearch  DatabaseType = 1 + iota //Provide int values to each of constants
+	Elasticsearch DatabaseType = 1 + iota //Provide int values to each of constants
 	Dynamo
 	Mysql
 )
 
 //Enums should be able to print as strings, so we declare slice of strings
-var database = [...]string {
+var database = [...]string{
 	"Elasticsearch",
 	"Dynamo",
 	"Mysql",
 }
 
-
 func (db DatabaseType) String() string {
-	return database[db -1]
+	return database[db-1]
 }
 
 func (typedb DatabaseType) GetClient() (interface{}, error) {
-	if typedb == Elasticsearch{
+	if typedb == Elasticsearch {
 		client, err := elastic.NewClient()
 		return client, err
 	}
 	fmt.Println("No such Database", typedb)
-	return nil,nil
+	return nil, nil
 }
 
 func GetWriter() (*csv.Writer, error) {
@@ -76,13 +75,13 @@ func GetWriter() (*csv.Writer, error) {
 }
 
 func (c Tweet) CsvWriter(writer *csv.Writer, m chan Tweet) {
-    var mutex = &sync.Mutex{}
+	var mutex = &sync.Mutex{}
 	for i := range m {
-        c = i
+		c = i
 		//fmt.Println(c)
 		data := []string{c.User, c.Post_date, c.Message}
 		//Introduced locks for write to csv file
-		mutex.Lock() 
+		mutex.Lock()
 		writer.Write(data)
 		writer.Flush()
 		mutex.Unlock()
@@ -92,26 +91,26 @@ func (c Tweet) CsvWriter(writer *csv.Writer, m chan Tweet) {
 
 //Now Generic lookup is possible thanks to this function
 func GetField(v *Tweet, field string) string {
-    r := reflect.ValueOf(v)
-    f := reflect.Indirect(r).FieldByName(field)
-   // fmt.Println(string(f.String()))
-    return string(f.String())
+	r := reflect.ValueOf(v)
+	f := reflect.Indirect(r).FieldByName(field)
+	// fmt.Println(string(f.String()))
+	return string(f.String())
 }
 
-//gets the searchresult and filters them for writing to csv 
-func Filtering(search chan *elastic.SearchResult)  {
+//gets the searchresult and filters them for writing to csv
+func Filtering(search chan *elastic.SearchResult) {
 	var t Tweet
 	var data chan Tweet = make(chan Tweet)
 	writer, err := GetWriter()
 	CheckError(err)
-	go t.CsvWriter(writer, data)  // spawning the csvwriter routine
+	go t.CsvWriter(writer, data) // spawning the csvwriter routine
 	for i := range search {
 		searchResult := i
 		for _, hit := range searchResult.Hits.Hits {
-	        err := json.Unmarshal(*hit.Source, &t)
+			err := json.Unmarshal(*hit.Source, &t)
 			CheckError(err)
 			//Filtering data
-			q.filter[0].Qkey = strings.Replace(q.filter[0].Qkey,q.filter[0].Qkey[:1], strings.ToUpper(q.filter[0].Qkey[:1]),1)
+			q.filter[0].Qkey = strings.Replace(q.filter[0].Qkey, q.filter[0].Qkey[:1], strings.ToUpper(q.filter[0].Qkey[:1]), 1)
 			if GetField(&t, q.filter[0].Qkey) == q.filter[0].Qvalue {
 				fmt.Println(t)
 				data <- t
@@ -125,7 +124,7 @@ func Filtering(search chan *elastic.SearchResult)  {
 func GetReportEL(client *elastic.Client) {
 	result := make(chan *elastic.SearchResult)
 	// spawinng the Filtering routine
-	go Filtering(result) 
+	go Filtering(result)
 	// the termquery uses all lower but for matching to filter exactly we have to convert the first letter to upper
 	boolq := elastic.NewBoolQuery()
 	termQuery := boolq.Filter(elastic.NewTermQuery(q.filter[0].Qkey, q.filter[0].Qvalue))
@@ -159,7 +158,7 @@ func GetReportEL(client *elastic.Client) {
 	if pages <= 0 {
 		fmt.Println(pages, "Records found")
 	}
-	close(result)  //closing the channel
+	close(result) //closing the channel
 
 }
 
@@ -175,9 +174,8 @@ var q Filter // Global because it has to be used at different routines
 func main() {
 	var k Fpair
 	var str DatabaseType
-	fmt.Println("Select Database :->\n 1. For Elasticsearch\n", "2. For Dynamo\n","3. For Mysql\n", "Enter choice")
+	fmt.Println("Select Database :->\n 1. For Elasticsearch\n", "2. For Dynamo\n", "3. For Mysql\n", "Enter choice")
 	fmt.Scan(&str)
-	// fmt.Println(str)
 	// fmt.Println(reflect.TypeOf(str))
 	fmt.Println("Enter the search Field")
 	fmt.Scan(&k.Qkey)
@@ -185,16 +183,16 @@ func main() {
 	fmt.Println("Enter the search string")
 	fmt.Scan(&k.Qvalue)
 
-	q = Filter {filter :[]Fpair{k}}
+	q = Filter{filter: []Fpair{k}}
 	fmt.Println(q.filter[0])
 	client, err := str.GetClient()
 	CheckError(err)
-	//type assertion for getclient 
+	//type assertion for getclient
 	switch v := client.(type) {
-		case *elastic.Client:
-			fmt.Println("Calling With Elasticsearch Client")
-			GetReportEL(v)
-		default:
-			fmt.Println("No such Client available",v )
+	case *elastic.Client:
+		fmt.Println("Calling With Elasticsearch Client")
+		GetReportEL(v)
+	default:
+		fmt.Println("No such Client available", v)
 	}
 }
